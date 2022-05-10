4eea55ecb64ced7b6e58bea05413c7500dbf04e7
FaustoFaggion <faustofaggion@hotmail.com>
2022-05-10 17:56:11 -0300

    create function message to be able to create a control of death

--- libft/Makefile
+++ libft/Makefile
@@ -1,6 +1,6 @@
 NAME		:= libft.a
 
-CC			:= clang
+CC			:= gcc
 
 CFLAGS		:= -Wall -Werror -Wextra
 
--- src/exec/lunch.c
+++ src/exec/lunch.c
@@ -6,18 +6,19 @@
 /*   By: fausto <fausto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 06:56:36 by fagiusep          #+#    #+#             */
-/*   Updated: 2022/05/10 17:37:15 by fausto           ###   ########.fr       */
+/*   Updated: 2022/05/10 17:54:57 by fausto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
 static void	has_taken_a_fork(t_seat *philo);
-static void	taken_fork(t_seat *philo, char c);
+static void	taken_fork(t_seat *philo);
 static void	is_eating(t_seat *philo);
 static void	is_sleeping(t_seat *philo);
 static void	is_thinking(t_seat *philo);
 static void	leave_fork(t_seat *philo);
+static void	msg(char *msg, t_seat *philo);
 
 void	*lunch(void *seat)
 {
@@ -44,13 +45,18 @@ void	*lunch(void *seat)
 
 static void	leave_fork(t_seat *philo)
 {
+	msg("leave fork\n",philo);
+	usleep(philo->routine.tim_slp * 1000);
+}
+
+static void	msg(char *msg, t_seat *philo)
+{
 	long int	t;
 	long int	now;
 	
 	t = get_time();
 	now = t - philo->routine.start;
-	printf("%ld %d leave fork\n",now, philo->id);
-	usleep(philo->routine.tim_slp * 1000);
+	printf("%ld %d %s\n", now, philo->id, msg);
 }
 
 static void	has_taken_a_fork(t_seat *philo)
@@ -58,54 +64,42 @@ static void	has_taken_a_fork(t_seat *philo)
 	if (philo->id % 2 != 0)
 	{
 		pthread_mutex_lock(philo->fork_right);
-		taken_fork(philo, 'L');
+		taken_fork(philo);
 		pthread_mutex_lock(philo->fork_left);
-		taken_fork(philo, 'R');
+		taken_fork(philo);
 	}
 	else
 	{
 		pthread_mutex_lock(philo->fork_left);
-		taken_fork(philo, 'R');
+		taken_fork(philo);
 		pthread_mutex_lock(philo->fork_right);
-		taken_fork(philo, 'L');
+		taken_fork(philo);
 	}
 }
 
-static void	taken_fork(t_seat *philo, char c)
-{
-	long int	t;
-	long int	now;
-	
-	t = get_time();
-	now = t - philo->routine.start;
-	printf("%ld %d has taken a fork %c\n", now, philo->id, c);
-	//printf("t: %ld start: %ld now: %ld %d has taken a fork %c\n", t, philo->routine.start, now, philo->id, c);
+static void	taken_fork(t_seat *philo)
+{	
+	msg("has taken a fork\n", philo);
 }
 
 static void	is_eating(t_seat *philo)
 {
 	long int	t;
-	long int	now;
+//	long int	now;
 	long int	die;
-	
-//	printf("%ld %d last meal\n",philo->routine.last_meal, philo->id);
+
 	t = get_time();
-	now = t - philo->routine.start;
+//	now = t - philo->routine.start;
 	die = t - philo->routine.last_meal;
 	printf("%ld %d die ...................................\n",die, philo->id);
-	printf("%ld %d is eating\n",now, philo->id);
+	msg("is eating\n", philo);
 	usleep(philo->routine.tim_eat * 1000);
 	philo->routine.last_meal = get_time();
 }
 
 static void	is_sleeping(t_seat *philo)
 {
-	long int	t;
-	long int	now;
-	
-	t = get_time();
-	now = t - philo->routine.start;
-	printf("%ld %d is sleeping\n",now, philo->id);
+	msg("is sleeping\n", philo);
 	usleep(philo->routine.tim_slp * 1000);
 }
 
@@ -114,7 +108,7 @@ static void	is_thinking(t_seat *philo)
 	long int	now;
 
 	now = get_now(philo);
-	printf("%ld %d is thinking\n",now, philo->id);
+	msg("is thinking\n",philo);
 	while (1)
 	{
 		now = get_time();
